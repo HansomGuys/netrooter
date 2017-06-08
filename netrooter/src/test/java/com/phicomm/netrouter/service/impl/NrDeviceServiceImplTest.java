478d5e36aef483bf7398842534b93cc1464e428d
chenglin <chenglin02.wang@phicomm.com>
2017-06-08 15:25:51 +0800

    package name and project name have been changed from netrooter to netrouter.

--- netrooter/src/test/java/com/phicomm/netrouter/service/impl/NrDeviceServiceImplTest.java
+++ netrooter/src/test/java/com/phicomm/netrouter/service/impl/NrDeviceServiceImplTest.java
@@ -1,4 +1,4 @@
-package com.phicomm.netrooter.service.impl;
+package com.phicomm.netrouter.service.impl;
 
 import org.apache.log4j.Logger;
 import org.junit.Test;
@@ -8,8 +8,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.alibaba.fastjson.JSON;
-import com.phicomm.netrooter.model.NrDevice;
-import com.phicomm.netrooter.service.NrDeviceService;
+import com.phicomm.netrouter.model.NrDevice;
+import com.phicomm.netrouter.service.NrDeviceService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring.xml", "classpath:spring-mybatis.xml" })
