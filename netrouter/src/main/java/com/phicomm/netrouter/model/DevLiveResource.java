f0548aa5f3a70099ef742e1ccae31d6a11043a9e
lpsunny <lp_sunny@163.com>
2017-06-25 19:52:04 +0800

    modify model folder
    modify model folder

--- netrouter/src/main/java/com/phicomm/netrouter/model/DevLiveResource.java
+++ netrouter/src/main/java/com/phicomm/netrouter/model/DevLiveResource.java
@@ -1,5 +1,7 @@
 package com.phicomm.netrouter.model;
 
+import java.util.Date;
+
 public class DevLiveResource extends DevLiveResourceKey {
     private String url;
 
@@ -8,6 +10,12 @@ public class DevLiveResource extends DevLiveResourceKey {
     private Integer maxslavecnt;
 
     private Integer currentslavecnt;
+
+    private Boolean online;
+
+    private Date firsttime;
+
+    private Date lasttime;
 
     public String getUrl() {
         return url;
@@ -39,5 +47,29 @@ public class DevLiveResource extends DevLiveResourceKey {
 
     public void setCurrentslavecnt(Integer currentslavecnt) {
         this.currentslavecnt = currentslavecnt;
+    }
+
+    public Boolean getOnline() {
+        return online;
+    }
+
+    public void setOnline(Boolean online) {
+        this.online = online;
+    }
+
+    public Date getFirsttime() {
+        return firsttime;
+    }
+
+    public void setFirsttime(Date firsttime) {
+        this.firsttime = firsttime;
+    }
+
+    public Date getLasttime() {
+        return lasttime;
+    }
+
+    public void setLasttime(Date lasttime) {
+        this.lasttime = lasttime;
     }
 }
