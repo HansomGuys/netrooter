58e35103239b6a50b549dcb6c54b054af3bc901e
lpsunny <lp_sunny@163.com>
2017-06-26 09:57:45 +0800

    modify service.impl folder
    modify service.impl folder

---
+++ netrouter/src/main/java/com/phicomm/netrouter/service/impl/IotDeviceServiceImpl.java
@@ -0,0 +1,21 @@
+package com.phicomm.netrouter.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.phicomm.netrouter.dao.IotDeviceMapper;
+import com.phicomm.netrouter.model.IotDevice;
+import com.phicomm.netrouter.service.IotDeviceService;
+
+@Service("iotDeviceService")
+public class IotDeviceServiceImpl implements IotDeviceService {
+
+	@Autowired
+	private IotDeviceMapper nrDeviceMapper;
+	@Override
+	public IotDevice getDeviceByPrimaryKey(long deviceid) {
+		// TODO Auto-generated method stub
+		return nrDeviceMapper.selectByPrimaryKey(deviceid);
+	}
+
+}
