1130e431a0f2073588d144c8ff8b0bf5ae2da223
chenglin <chenglin02.wang@phicomm.com>
2017-07-10 14:08:55 +0800

    Using sharedLock to stead the time scope in SharedStrategy

--- netrouter/src/main/java/com/phicomm/netrouter/model/SharedStrategy.java
+++ netrouter/src/main/java/com/phicomm/netrouter/model/SharedStrategy.java
@@ -1,12 +1,11 @@
 package com.phicomm.netrouter.model;
 
-import java.util.Date;
-
 public class SharedStrategy {
-	private Date startSharedTime;
-	private Date endSharedTime;
+//	private Date startSharedTime;
+//	private Date endSharedTime;
 	private int sharedBW;
-	public Date getStartSharedTime() {
+	private boolean sharedLock;
+	/*public Date getStartSharedTime() {
 		return startSharedTime;
 	}
 	public void setStartSharedTime(Date startSharedTime) {
@@ -17,11 +16,17 @@ public class SharedStrategy {
 	}
 	public void setEndSharedTime(Date endSharedTime) {
 		this.endSharedTime = endSharedTime;
-	}
+	}*/
 	public int getSharedBW() {
 		return sharedBW;
 	}
 	public void setSharedBW(int sharedBW) {
 		this.sharedBW = sharedBW;
+	}
+	public boolean isSharedLock() {
+		return sharedLock;
+	}
+	public void setSharedLock(boolean sharedLock) {
+		this.sharedLock = sharedLock;
 	}
 }
